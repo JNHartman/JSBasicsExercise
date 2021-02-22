cca1b3721ef3b459b0c2075f20b9807e7b255466
Jesse Hartman <jessenhartman@gmail.com>
2021-02-22 14:11:59 -0600

    primary commit

---
+++ jsBasicsExercise.js
@@ -0,0 +1,70 @@
+//#1 Use a console log to have your favorite number display in the console/terminal
+console.log(21);
+
+//#2 Use a comment to type out the 5 main data types (AKA Primitives)
+//strings
+// "This Is The Way";
+
+
+// numbers
+// 21;
+
+
+// booleans
+// true;
+// false;
+
+
+// undefined
+// undefined;
+
+
+// null.
+// null;
+
+// 3
+// Addition is +, Subtraction is -, Multiplication *, Division is /, Exponentiation is **, Modulus is %
+
+// 4
+console.log(5%2);
+console.log(5%4);
+
+// 5
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+// 6
+console.log("Wanda" + "Vision");
+
+// 7
+console.log("Jesse".length + "Hartman".length);
+
+// 8
+console.log("Avengers: Endgame" [16]);
+
+// Bonus
+// 1
+console.log("\\");
+
+// 2
+console.log(5 ** 4 - 9);
+
+// 3
+console.log("I".length % "Am".length - "Iron".length % "Man".length);
+console.log("I".length - "Am".length + "Iron".length - "Man".length);
+console.log("I".length + "Am".length + ("Iron".length % 2) - "Man".length);
+
+// 4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("HAWKEYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// 5
+var m = 10;
+var c = 5;
+var u = m + c;
+console.log(u);
